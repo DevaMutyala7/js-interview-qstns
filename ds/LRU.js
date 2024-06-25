ea45412e55a71b328d3c928cfc6a7cace306cd11
Deva <82375896+DevaMutyala7@users.noreply.github.com>
2024-06-25 11:46:33 +0530

    lru cache refactored

--- ds/LRU.js
+++ ds/LRU.js
@@ -1,142 +1,67 @@
 class Node {
-  constructor(val) {
-    this.elem = val;
-    this.next = null;
+  constructor(key, val) {
+    this.val = val;
+    this.key = key;
     this.prev = null;
-  }
-}
-
-class Dll {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  findNode(node) {
-    let nodeToBeSearched = this.head;
-    while (nodeToBeSearched !== node) {
-      nodeToBeSearched = nodeToBeSearched.next;
-    }
-
-    return nodeToBeSearched;
-  }
-
-  insertAtEnd(elem) {
-    let node = new Node(elem);
-    if (!this.size) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      let oldTail = this.tail;
-      this.tail.next = node;
-      this.tail = node;
-      this.tail.prev = oldTail;
-    }
-    this.size++;
-    return node;
-  }
-
-  insertAtStart(val) {
-    let newNode = new Node(val);
-    let prevNode = this.head;
-    newNode.next = prevNode;
-    prevNode.prev = newNode;
-    this.head = newNode;
-    this.size++;
-  }
-
-  deleteAtStart() {
-    let node = this.head;
-    this.head = node.next;
-    this.head.prev = null;
-    this.size--;
-    return node;
-  }
-
-  deleteAtEnd() {
-    let before = this.tail.prev;
-    this.tail = before;
-    this.tail.next = null;
-    this.size--;
-    return before;
-  }
-
-  deleteNode(node) {
-    let nodeToBeDeleted = this.findNode(node);
-
-    if (nodeToBeDeleted === this.head) {
-      this.head = this.head.next;
-      this.head.prev = null;
-    } else if (nodeToBeDeleted === this.tail) {
-      this.tail = this.tail.prev;
-      this.tail.next = null;
-    } else {
-      let prev = nodeToBeDeleted.prev;
-      let next = nodeToBeDeleted.next;
-      prev.next = next;
-      next.prev = prev;
-    }
-
-    return nodeToBeDeleted;
-  }
-
-  size() {
-    return this.size;
-  }
-
-  listAll() {
-    let node = this.head;
-    let arr = [];
-    while (node) {
-      arr.push(node.elem);
-      node = node.next;
-    }
-
-    console.log(arr);
+    this.next = null;
   }
 }
 
 class LRU {
+  head = new Node(0, 0);
+  tail = new Node(0, 0);
   constructor(size) {
     this.size = size;
-    this.dll = new Dll();
+    this.head.next = this.tail;
     this.hashMap = new Map();
   }
 
-  put(key, val) {
-    if (this.hashMap.size >= this.size) {
-      let deletedLRU = this.deleteLRU();
-      this.hashMap.delete(deletedLRU.elem.key);
-    }
-    let nodeAddress = this.dll.insertAtEnd({ key, value: val });
-    this.hashMap.set(key, nodeAddress);
-  }
-
-  deleteLRU() {
-    return this.dll.deleteAtStart();
-  }
-
   get(key) {
-    let item = this.hashMap.get(key).elem.value;
     if (this.hashMap.get(key)) {
-      this.use(key, this.hashMap.get(key));
-      return item;
+      let node = this.hashMap.get(key);
+      this.remove(node);
+      this.insert(node);
+      return node.val;
     }
     return -1;
   }
 
-  use(key, node) {
-    let deleted = this.dll.deleteNode(node);
-    let inserted = this.dll.insertAtEnd({
-      key: key,
-      value: deleted.elem.value,
-    });
-    this.hashMap.set(key, inserted);
+  put(key, val) {
+    if (this.hashMap.get(key)) {
+      this.remove(this.hashMap.get(key));
+    }
+    if (this.hashMap.size >= this.size) {
+      this.remove(this.tail.prev);
+    }
+    let newNode = new Node(key, val);
+    this.insert(newNode);
+  }
+
+  remove(node) {
+    this.hashMap.delete(node.key);
+    let prev = node.prev;
+    let next = node.next;
+    prev.next = next;
+    next.prev = prev;
+  }
+
+  insert(node) {
+    this.hashMap.set(node.key, node);
+    let next = this.head.next;
+    this.head.next = node;
+    node.prev = this.head;
+    node.next = next;
+    next.prev = node;
   }
 
   display() {
-    this.dll.listAll();
+    let res = [];
+    let node = this.head;
+    while (node) {
+      res.push(node.val);
+      node = node.next;
+    }
+    console.log(res);
   }
 }
 
@@ -159,3 +84,4 @@ lru.put(6, 60);
 lru.display();
 console.log("-------------");
 console.log("get", lru.get(5));
+lru.display();
