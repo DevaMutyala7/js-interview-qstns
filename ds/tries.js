43632c150e12cce6be6bf7e8f9df6aca7b2d1cc3
Deva <82375896+DevaMutyala7@users.noreply.github.com>
2024-11-08 11:17:27 +0530

    remove and find methods implemented in trie

--- ds/tries.js
+++ ds/tries.js
@@ -47,8 +47,77 @@ class Trie {
         current = current.children[idx];
       }
     }
-
     return true;
+  }
+
+  remove(word) {
+    let prevEow;
+    let current = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let char = word.charAt(i);
+      let idx = char.charCodeAt(0) - "a".charCodeAt(0);
+
+      if (current.children[idx]) {
+        current = current.children[idx];
+        if (i !== word.length - 1 && current.eow) {
+          prevEow = current;
+        }
+      } else {
+        return "no such word";
+      }
+
+      if (i === word.length - 1) {
+        if (current.eow) {
+          let nextElements = current.children.some((item) => item !== null);
+          if (nextElements) {
+            current.eow = false;
+          } else {
+            if (prevEow) {
+              prevEow.children.fill(null);
+            }
+          }
+        } else {
+          return "No such word";
+        }
+      }
+    }
+    return "removed successfully";
+  }
+
+  getWords(prefix, current, word, char) {
+    current.children.forEach((node) => {
+      if (node) {
+        let newChar = char + node.char;
+        if (node.eow) {
+          word.push(prefix + newChar);
+        } else {
+          this.getWords(prefix, node, word, newChar);
+        }
+      }
+    });
+  }
+
+  find(prefix) {
+    let words = [];
+    let current = this.root;
+    for (let i = 0; i < prefix.length; i++) {
+      let char = prefix.charAt(i);
+      let idx = char.charCodeAt(0) - "a".charCodeAt(0);
+
+      if (!current.children[idx] || current.children[idx].char !== char) {
+        return "No such word";
+      } else {
+        current = current.children[idx];
+      }
+    }
+
+    if (current.eow) {
+      words.push(prefix);
+    }
+
+    this.getWords(prefix, current, words, "");
+
+    return words;
   }
 }
 
@@ -56,6 +125,8 @@ const tree = new Trie();
 
 tree.insert("apple");
 tree.insert("battery");
+tree.insert("batman");
 tree.insert("bat");
+tree.insert("bathtub");
 
-console.log(tree.search("battery"));
+console.log(tree.find("bat"));
